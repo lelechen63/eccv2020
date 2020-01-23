30a0986c19b1b49c1c1ebc803c96251b9a2b083f
Lele Chen <lchen63@ur.rochester.edu>
2020-01-23 11:31:01 -0500

    add foceforecen get data for oppo

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 # train on foceforensic dataset using base model
 
-CUDA_VISIBLE_DEVICES=3 python base_train.py --name base_facefor --dataname=facefor --dataroot /home/cxu-serve/p1/common/faceforensics/original_sequences/youtube
+CUDA_VISIBLE_DEVICES=3 python base_train.py --name base_facefor --dataname=facefor --debug --dataroot /mnt/Data/lchen63/faceforensics/original_sequences
 
 # train on lrs dataset using base model
 
