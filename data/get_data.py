8e76c961213d4c0c228502e363c7740077f130ef
陈乐乐 <vincentchen@chenyueles-MacBook-Air.local>
2020-02-02 16:42:31 -0500

    debug

--- data/get_data.py
+++ data/get_data.py
@@ -162,7 +162,7 @@ def prepare_data_grid():
                 os.mkdir("./tmp01")
                 for ii in range(lmark1.shape[0]):
                     img = real_video[ii]
-                    print ('_++++++++++++' , openrate(lmark1[ii]))
+                    print ('_++++++++++++' ，str(ii) , openrate(lmark1[ii]))
                     for jj in range(68):
                         x=int(lmark1[ii][jj][1])
                         y =int(lmark1[ii][jj][0])
