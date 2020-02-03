9fed37cf2caceca96aa310e30cad90e64e1977e3
陈乐乐 <vincentchen@chenyueles-MacBook-Air.local>
2020-02-03 00:14:28 -0500

    for demo atnet

--- raw2lmark_demo.py
+++ raw2lmark_demo.py
@@ -178,7 +178,7 @@ def test():
     config.cuda1 = torch.device('cuda:0')
 
     _ , lmark = preprocess_img(config.person)
-    data_batch = get_demo_batch(config.in_file, lmark)
+    example_landmark,  chunks = get_demo_batch(config.in_file, lmark)
 
     generator = SPCH2FLM2()
     device_ids = [int(i) for i in config.device_ids.split(',')]
@@ -190,85 +190,10 @@ def test():
     if os.path.exists('./temp'):
         shutil.rmtree('./temp')
     os.mkdir('./temp')
-    os.mkdir('./temp/img')
-    os.mkdir('./temp/motion')
-    os.mkdir('./temp/attention')
+        
     
-    test_file = config.in_file
+    sound, _ = librosa.load(config.in_file, sr=44100)
 
-    
-    example_image = cv2.cvtColor(example_image, cv2.COLOR_BGR2RGB)
-    example_image = transform(example_image)
-
-    example_landmark =  example_landmark.reshape((1,example_landmark.shape[0]* example_landmark.shape[1]))
-
-    if config.cuda:
-        example_image = Variable(example_image.view(1,3,128,128)).cuda()
-        example_landmark = Variable(torch.FloatTensor(example_landmark.astype(float)) ).cuda()
-    else:
-        example_image = Variable(example_image.view(1,3,128,128))
-        example_landmark = Variable(torch.FloatTensor(example_landmark.astype(float)))
-    # Load speech and extract features
-    example_landmark = example_landmark * 5.0
-    example_landmark  = example_landmark - mean.expand_as(example_landmark)
-    example_landmark = torch.mm(example_landmark,  pca)
-    speech, sr = librosa.load(test_file, sr=16000)
-    mfcc = python_speech_features.mfcc(speech ,16000,winstep=0.01)
-    speech = np.insert(speech, 0, np.zeros(1920))
-    speech = np.append(speech, np.zeros(1920))
-    mfcc = python_speech_features.mfcc(speech,16000,winstep=0.01)
-
-    sound, _ = librosa.load(test_file, sr=44100)
-
-    print ('=======================================')
-    print ('Start to generate images')
-    t =time.time()
-    ind = 3
-    with torch.no_grad(): 
-        fake_lmark = []
-        input_mfcc = []
-        while ind <= int(mfcc.shape[0]/4) - 4:
-            t_mfcc =mfcc[( ind - 3)*4: (ind + 4)*4, 1:]
-            t_mfcc = torch.FloatTensor(t_mfcc).cuda()
-            input_mfcc.append(t_mfcc)
-            ind += 1
-        input_mfcc = torch.stack(input_mfcc,dim = 0)
-        input_mfcc = input_mfcc.unsqueeze(0)
-        fake_lmark = encoder(example_landmark, input_mfcc)
-        fake_lmark = fake_lmark.view(fake_lmark.size(0) *fake_lmark.size(1) , 6)
-        example_landmark  = torch.mm( example_landmark, pca.t() ) 
-        example_landmark = example_landmark + mean.expand_as(example_landmark)
-        fake_lmark[:, 1:6] *= 2*torch.FloatTensor(np.array([1.1, 1.2, 1.3, 1.4, 1.5])).cuda() 
-        fake_lmark = torch.mm( fake_lmark, pca.t() )
-        fake_lmark = fake_lmark + mean.expand_as(fake_lmark)
-    
-
-        fake_lmark = fake_lmark.unsqueeze(0) 
-
-        fake_ims, atts ,ms ,_ = decoder(example_image, fake_lmark, example_landmark )
-
-        for indx in range(fake_ims.size(1)):
-            fake_im = fake_ims[:,indx]
-            fake_store = fake_im.permute(0,2,3,1).data.cpu().numpy()[0]
-            scipy.misc.imsave("{}/{:05d}.png".format(os.path.join('../', 'temp', 'img') ,indx ), fake_store)
-            m = ms[:,indx]
-            att = atts[:,indx]
-            m = m.permute(0,2,3,1).data.cpu().numpy()[0]
-            att = att.data.cpu().numpy()[0,0]
-
-            scipy.misc.imsave("{}/{:05d}.png".format(os.path.join('../', 'temp', 'motion' ) ,indx ), m)
-            scipy.misc.imsave("{}/{:05d}.png".format(os.path.join('../', 'temp', 'attention') ,indx ), att)
-
-        print ( 'In total, generate {:d} images, cost time: {:03f} seconds'.format(fake_ims.size(1), time.time() - t) )
-            
-        fake_lmark = fake_lmark.data.cpu().numpy()
-        np.save( os.path.join( config.sample_dir,  'obama_fake.npy'), fake_lmark)
-        fake_lmark = np.reshape(fake_lmark, (fake_lmark.shape[1], 68, 2))
-        utils.write_video_wpts_wsound(fake_lmark, sound, 44100, config.sample_dir, 'fake', [-1.0, 1.0], [-1.0, 1.0])
-        video_name = os.path.join(config.sample_dir , 'results.mp4')
-        utils.image_to_video(os.path.join('../', 'temp', 'img'), video_name )
-        utils.add_audio(video_name, config.in_file)
-        print ('The generated video is: {}'.format(os.path.join(config.sample_dir , 'results.mov')))
         
 
 # test()
