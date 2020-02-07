2aa94fa61660840cc0e65766aaa6a25eb0506651
Lele Chen <lchen63@ur.rochester.edu>
2020-02-06 22:21:55 -0500

    h

--- data/voxceleb2lrs.py
+++ data/voxceleb2lrs.py
@@ -120,19 +120,20 @@ def align_videos(config):
             all_files  =  os.listdir( os.path.join( root_path , identities[index], v_id))
             for ff in all_files:
                 if ff[-3:] == 'npy' and len(ff) == 9:
-                    try:
-                        ori_video_path =  os.path.join( root_path , identities[index], v_id, ff[:-4] + '.mp4')
-                        ani_video_path = os.path.join( root_path , identities[index], v_id, ff[:-4] + '_ani.mp4')
-                        lmark_path = os.path.join( root_path , identities[index], v_id, ff)
-                        
-                        if os.path.exists(ori_video_path[:-4] + '_aligned.mp4'):
-                            continue
-                        if os.path.exists(ori_video_path) and os.path.exists(ani_video_path) and os.path.exists(lmark_path) :
-                            _crop_video(ori_video_path, ani_video_path, lmark_path )
-                    except:
-                        print  (ori_video_path)
+                    # try:
+                    ori_video_path =  os.path.join( root_path , identities[index], v_id, ff[:-4] + '.mp4')
+                    ani_video_path = os.path.join( root_path , identities[index], v_id, ff[:-4] + '_ani.mp4')
+                    lmark_path = os.path.join( root_path , identities[index], v_id, ff)
+                    
+                    if os.path.exists(ori_video_path[:-4] + '_aligned.mp4'):
                         continue
-
+                    if os.path.exists(ori_video_path) and os.path.exists(ani_video_path) and os.path.exists(lmark_path) :
+                        _crop_video(ori_video_path, ani_video_path, lmark_path )
+                    # except:
+                    #     print  (ori_video_path)
+                    #     continue
+                else:
+                    print (ff)
 def RT_compute(config):
     consider_key = [1,2,3,4,5,11,12,13,14,15,27,28,29,30,31,32,33,34,35,39,42,36,45,17,21,22,26]
     root_path = '/mnt/Data/lchen63/voxceleb2/unzip/dev_video'
@@ -184,8 +185,8 @@ def RT_compute(config):
                     # except:
                     #     print  (front_path)
                     #     continue
-# align_videos(config)
-RT_compute(config)
+align_videos(config)
+# RT_compute(config)
 
 def prepare_standard1():  # get cropped image by input the reference image
     img_path = '/home/cxu-serve/p1/lchen63/voxceleb/unzip/tmp/tmp/00001_00030.png'
