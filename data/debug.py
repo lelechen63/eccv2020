3f64c6a366c66848812f685cf2b8ed9ddbe09621
Lele Chen <lchen63@ur.rochester.edu>
2020-02-10 10:11:31 -0500

    g

--- data/debug.py
+++ data/debug.py
@@ -1,5 +1,17 @@
 import os 
 from scipy.io import wavfile
+
+def tmp():
+    path = '/data/lchen63/vox_obj/vox'
+    ff = os.listdir(path)
+    for f in ff:
+        gg = f.split('__')
+        new_path = os.path.join( path , gg[0] , gg[1], gg[2], gg[3])
+        old_path = os.path.join(path , f)
+        command = 'mv ' + old_path +' ' + new_path
+        print (command)
+        # os.system(command)
+tmp()
 # path = '/home/cxu-serve/p1/common/grid/audio/s1'
 # for i in os.listdir(path):
 #     fs, data = wavfile.read(  os.path.join( path , i ))
