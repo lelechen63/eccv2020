cf6b009f3fd3e077f0d40fa23aab0ac882bf872f
陈乐乐 <vincentchen@chenyueles-MacBook-Air.local>
2020-04-28 16:15:45 -0400

    process obama

--- data/obama_head_fake.py
+++ data/obama_head_fake.py
@@ -73,8 +73,8 @@ def landmark_extractor():
     frames = read_videos(original_video_path)
     print (len(frames))
     for i in range(len(frames)):
-        fake_frame = frames[i, 256 *2 ：256 *3 ]
-        gt_frame = frames[i, 256 * 1：256 *2 ]
+        fake_frame = frames[i, 256 *2 :256 *3 ]
+        gt_frame = frames[i, 256 * 1:256 *2 ]
         cv2.imwrite(os.path.join('/home/cxu-serve/p1/common/other/obama_fake',  method , 'tmp', '%05d.jpg'%i),fake_frame)
 
         cv2.imwrite(os.path.join('/home/cxu-serve/p1/common/other/obama_fake',  'gt' , 'tmp', '%05d.jpg'%i),gt_frame)
