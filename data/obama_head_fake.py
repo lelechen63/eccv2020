bbe2c21c4233f7fcd1ced5ccd82ccdf3909bdb81
陈乐乐 <vincentchen@chenyueles-MacBook-Air.local>
2020-04-28 16:14:30 -0400

    process obama

--- data/obama_head_fake.py
+++ data/obama_head_fake.py
@@ -7,7 +7,6 @@ import face_alignment
 import numpy as np
 import cv2
 from face_tracker import _crop_video
-from utils import face_utils, util
 from scipy.spatial.transform import Rotation 
 from scipy.io import wavfile
 import torch
@@ -38,50 +37,76 @@ def read_videos( video_path):
 
     return real_video
 
+def read_videos( video_path):
+    cap = cv2.VideoCapture(video_path)
+    real_video = []
+    while(cap.isOpened()):
+        ret, frame = cap.read()
+        if ret == True:
+            real_video.append(frame)
+        else:
+            break
+
+    return real_video
+
 def landmark_extractor():
     fa = face_alignment.FaceAlignment(face_alignment.LandmarksType._3D, device='cuda:0')
-    root_path = '/home/cxu-serve/p1/common/Obama'
-    train_list = sorted(os.listdir(os.path.join(root_path, 'video')))
-    batch_length =  int(0.2 * len(train_list))
-    for i in tqdm(range(batch_length * (config.batch_id -1), batch_length * (config.batch_id ))):
-        p_id = train_list[i]
-        if 'crop' in p_id or p_id[-3:] == 'npy':
-            continue
+    # root_path = '/home/cxu-serve/p1/common/Obama'
+    # train_list = sorted(os.listdir(os.path.join(root_path, 'video')))
+    # batch_length =  int(0.2 * len(train_list))
+    # for i in tqdm(range(batch_length * (config.batch_id -1), batch_length * (config.batch_id ))):
+    #     p_id = train_list[i]
+    #     if 'crop' in p_id or p_id[-3:] == 'npy':
+    #         continue
+    method = 'baseline'
+    if not os.path.exists(os.path.join('/home/cxu-serve/p1/common/other/obama_fake',  method , 'tmp')):
+        os.mkdir(os.path.join('/home/cxu-serve/p1/common/other/obama_fake',  method , 'tmp'))
 
-        original_video_path = os.path.join(root_path,  'video',  p_id)
-        lmark_path = os.path.join(root_path,  'video',  p_id[:-4] + '__original2.npy')            
-        print (original_video_path)
-        cropped_video_path = os.path.join(root_path,  'video',   p_id[:-4] + '__crop2.mp4')
+    original_video_path = os.path.join('/home/cxu-serve/p1/common/other/obama_fake',  method,  'test.mp4')
+
+    cropped_video_path = original_video_path[:-4] +'_crop.mp4'
+
+
+    lmark_path = cropped_video_path[:-3] + 'npy'           
+    print (original_video_path)
+    
+    frames = read_videos(original_video_path)
+    print (len(frames))
+    for i in range(len(frames)):
+        fake_frame = frames[i, 256 *2 ：256 *3 ]
+        gt_frame = frames[i, 256 * 1：256 *2 ]
+        cv2.imwrite(os.path.join('/home/cxu-serve/p1/common/other/obama_fake',  method , 'tmp', '%05d.jpg'%i),fake_frame)
+
+        cv2.imwrite(os.path.join('/home/cxu-serve/p1/common/other/obama_fake',  'gt' , 'tmp', '%05d.jpg'%i),gt_frame)
+
+    # if os.path.exists(lmark_path):
+        # continue
         
-        # if os.path.exists(lmark_path):
-            # continue
-            
-        try:
-            _crop_video(original_video_path, config.batch_id,  1)
-            
-            command = 'ffmpeg -framerate 25  -i ./temp%05d'%config.batch_id + '/%05d.png  -vcodec libx264  -vf format=yuv420p -y ' +  cropped_video_path
-            os.system(command)
-            cap = cv2.VideoCapture(cropped_video_path)
-            lmark = []
-            while(cap.isOpened()):
-                # counter += 1 
-                # if counter == 5:
-                #     break
-                ret, frame = cap.read()
-                if ret == True:
-                    frame = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB )
+        # _crop_video(original_video_path, config.batch_id,  1)
+        
+    command1 = 'ffmpeg -framerate 25  -i '    + os.path.join('/home/cxu-serve/p1/common/other/obama_fake',  method , 'tmp', '%05d.jpg' )+'  -vcodec libx264  -vf format=yuv420p -y ' +  cropped_video_path
+    command2 = 'ffmpeg -framerate 25  -i '    + os.path.join('/home/cxu-serve/p1/common/other/obama_fake',  'gt' , 'tmp', '%05d.jpg' )+'  -vcodec libx264  -vf format=yuv420p -y ' +   os.path.join('/home/cxu-serve/p1/common/other/obama_fake',  'gt',  'test_crop.mp4')
+    print (command1)
+    print (command2)
+    # os.system(command)
+    # cap = cv2.VideoCapture(cropped_video_path)
+    # lmark = []
+    # while(cap.isOpened()):
+    #     # counter += 1 
+    #     # if counter == 5:
+    #     #     break
+    #     ret, frame = cap.read()
+    #     if ret == True:
+    #         frame = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB )
 
-                    preds = fa.get_landmarks(frame)[0]
-                    lmark.append(preds)
-                else:
-                    break
-                    
-            lmark = np.asarray(lmark)
-            np.save(lmark_path, lmark)
-        except:
-            print (cropped_video_path)
+    #         preds = fa.get_landmarks(frame)[0]
+    #         lmark.append(preds)
+    #     else:
+    #         break
+            
+    # lmark = np.asarray(lmark)
+    # np.save(lmark_path, lmark)
 
-            continue
 
 def RT_compute():
     consider_key = [1,2,3,4,5,11,12,13,14,15,27,28,29,30,31,32,33,34,35,39,42,36,45,17,21,22,26]
@@ -141,7 +166,6 @@ def RT_compute():
 import torch
 import random
 from sklearn.decomposition import PCA
-from utils import face_utils
 
 def openrate(lmark1):
     open_pair = []
@@ -235,121 +259,6 @@ def get_front():
         pkl.dump(data, handle, protocol=pkl.HIGHEST_PROTOCOL)
 
 
-def swith_identity_obama(obamaid = '00025_aligned'):
-
-    # file:///home/cxu-serve/p1/common/voxceleb2/unzip/test_video/id04094/2sjuXzB2I1M/00025.mp4
-        # src_lmark_path = '/home/cxu-serve/p1/common/voxceleb2/unzip/test_video/id00817/HUmfsvegMRo/00175_aligned_front.npy' 
-        src_lmark_path = '/home/cxu-serve/p1/common/Obama/video/3_3__front2.npy' 
-        # tar_lmark_path =  '/home/cxu-serve/p1/common/demo/'  +  id + '_original_front.npy'
-        tar_lmark_path = '/home/cxu-serve/p1/common/demo/00025__lisa2__rotated.npy'
-
-        tar_front_lmark_path = '/home/cxu-serve/p1/common/demo/00025__lisa2__front.npy'
-        srt_rt_path = '/home/cxu-serve/p1/common/demo/00025_aligned_rt.npy'
-        print (srt_rt_path)
-        rt = np.load(srt_rt_path)
-        # rt = np.vstack([rt,rt[::-1,:],rt,rt[::-1,:],rt,rt[::-1,:],rt,rt[::-1,:],rt,rt[::-1,:],rt,rt[::-1,:]])
-
-        # np.save('/home/cxu-serve/p1/common/demo/3_3__rt2.npy', rt)
-
-        print (rt.shape)
-        
-        src_lmark = np.load(src_lmark_path)
-
-
-
-
-        
-        tar_lmark = np.load(tar_lmark_path)
-
-        # tar_lmark = np.vstack([tar_lmark,tar_lmark[::-1,:,:],tar_lmark,tar_lmark[::-1,:,:],tar_lmark,tar_lmark[::-1,:,:],tar_lmark,tar_lmark[::-1,:,:],tar_lmark,tar_lmark[::-1,:,:],tar_lmark,tar_lmark[::-1,:,:]])
-
-        # np.save('/home/cxu-serve/p1/common/demo/3_3__original2.npy', tar_lmark)
-        lmark_length = min(src_lmark.shape[0], tar_lmark.shape[0] )        
-        find_rt = []
-        for t in range(0, lmark_length):
-            find_rt.append(sum(np.absolute(rt[t,:3])))
-        find_rt = np.asarray(find_rt)
-
-
-        tar_front_lmark = np.load(tar_front_lmark_path)
-
-        # tar_front_lmark = np.vstack([tar_front_lmark,tar_front_lmark[::-1,:,:],tar_front_lmark,tar_front_lmark[::-1,:,:],tar_front_lmark,tar_front_lmark[::-1,:,:],tar_front_lmark,tar_front_lmark[::-1,:,:],tar_front_lmark,tar_front_lmark[::-1,:,:],tar_front_lmark,tar_front_lmark[::-1,:,:]])
-        # np.save('/home/cxu-serve/p1/common/demo/3_3__front2.npy', tar_front_lmark)
-        min_indexs =  np.argsort(find_rt)[:100]
-
-        for indx in min_indexs:
-            if  abs(openrate(tar_lmark[indx])) < 2:
-                min_index = indx
-                tempolate_openrate = openrate(tar_front_lmark[indx])
-                break
-
-        
-        current_tempolate =tar_front_lmark[min_index].copy()
-
-
-
-        for  i in range(lmark_length):
-            if abs(openrate(src_lmark[i])- tempolate_openrate) < 0.1:
-                src_id = i
-                break
-
-        print (min_index, src_id)
-
-        diff = current_tempolate[:,:2] -  src_lmark[src_id,:,:2]
-              
-
-        denormed_lamrk = tar_front_lmark[:lmark_length].copy()
-        
-        denormed_lamrk[:,48:,:2] = src_lmark[:,48:,:2] # + diff[48:]
-
-        rotated = np.zeros((lmark_length, 68 , 3))
-        for i in range(denormed_lamrk.shape[0]):
-            rotated[i] = util.reverse_rt(denormed_lamrk[i], rt[i])
-
-
-        np.save('/home/cxu-serve/p1/common/demo/demo_' + obamaid + '__rotated.npy', rotated)
-        np.save('/home/cxu-serve/p1/common/demo/demo_' + obamaid + '__front.npy', denormed_lamrk)
-
-
-
-def swith_identity(id = 'lisa2'):
-        src_lmark_path = '/home/cxu-serve/p1/common/demo/00025_aligned_front.npy' 
-
-        tar_lmark_path =  '/home/cxu-serve/p1/common/demo/'  +  id + '_original_front.npy'
-
-        srt_rt_path = src_lmark_path.replace('front', 'rt')
-        rt = np.load(srt_rt_path)
-        lmark_length = rt.shape[0]
-
-
-        src_lmark = np.load(src_lmark_path)
-
-        tar_lmark = np.load(tar_lmark_path)[0]
-
-
-        openrates = []
-        for  i in range(src_lmark.shape[0]):
-            openrates.append(openrate(src_lmark[i]))
-        openrates = np.asarray(openrates)
-        min_index = np.argmin(openrates)
-
-        print (min_index)
-
-        current_tempolate =src_lmark[min_index].copy()
-
-        diff = current_tempolate[:,:2] -  tar_lmark[:,:2]
-
-        denormed_lamrk = src_lmark.copy()
-        denormed_lamrk[:,:48,:2] = src_lmark[:,:48,:2]  - diff[:48]
-
-        rotated = np.zeros((denormed_lamrk.shape[0], 68 , 3))
-        for i in range(denormed_lamrk.shape[0]):
-            rotated[i] = util.reverse_rt(denormed_lamrk[i], rt[i])
-
-        np.save('/home/cxu-serve/p1/common/demo/00025__' + id + '__rotated.npy', rotated)
-        np.save('/home/cxu-serve/p1/common/demo/00025__' + id + '__front.npy', denormed_lamrk)
-
-
     #     find_rt = []
     #     for t in range(0, lmark_length):
     #         find_rt.append(sum(np.absolute(rt[t,:3])))
@@ -363,32 +272,6 @@ def swith_identity(id = 'lisa2'):
     # with open(os.path.join( root, 'pickle','train_lmark2img.pkl'), 'wb') as handle:
     #     pkl.dump(data, handle, protocol=pkl.HIGHEST_PROTOCOL)
 
-def diff():
-    root_path  = '/home/cxu-serve/p1/common/CREMA'
-    _file = open(os.path.join(root_path, 'pickle','train_lmark2img.pkl'), "rb")
-    datalist = pkl.load(_file)
-    _file.close()
-    batch_length = int( len(datalist))
-    landmarks = []
-    k = 20
-    norm_lmark = np.load('../basics/s1_pgbk6n_01.npy')[:,:2]
-   
-    for index in tqdm(range(batch_length)):
-        lmark_path = os.path.join(root_path,  'VideoFlash', datalist[index][0][:-10] +'_front.npy'  )
-        lmark = np.load(lmark_path)[:,:,:2]
-
-
-        openrates = []
-        for  i in range(lmark.shape[0]):
-            openrates.append(openrate(lmark[i]))
-        openrates = np.asarray(openrates)
-        min_index = np.argmin(openrates)
-        diff =  lmark[min_index] - norm_lmark
-        np.save(lmark_path[:-10] +'_%05d_diff.npy'%(min_index) , diff)
-        datalist[index].append(min_index) 
-
-    with open(os.path.join(root_path, 'pickle','train_lmark2img.pkl'), 'wb') as handle:
-        pkl.dump(datalist, handle, protocol=pkl.HIGHEST_PROTOCOL)
 # swith_identity('turing1')
 # swith_identity_obama()
 # get_front()
@@ -405,7 +288,7 @@ def diff():
 # data_original = np.dot(data_reduced,component) + mean
 # np.save( 'gg.npy', data_original )
 # print (data - data_original)
-# landmark_extractor()
+landmark_extractor()
 # 
 # RT_compute()
 # diff()
